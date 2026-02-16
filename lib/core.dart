89af38d3875cf16be763a56df1773b843154c138
Bakr Mohamed <bakrmohamed@Bakrs-Mac-mini.local>
2026-02-16 12:38:47 +0200

    Device Security Helper

--- lib/core.dart
+++ lib/core.dart
@@ -3,6 +3,7 @@ export 'src/design/typography/app_font_weight.dart';
 export 'src/design/typography/font_helper.dart';
 // Helpers
 export 'src/helpers/device_helper.dart';
+export 'src/helpers/device_security/device_security.dart';
 export 'src/helpers/enums.dart';
 export 'src/helpers/language_manager.dart';
 export 'src/helpers/models/custom_error.dart';
@@ -18,10 +19,10 @@ export 'src/helpers/network/ssl_pinning.dart';
 export 'src/helpers/preferences/annotations.dart';
 export 'src/helpers/preferences/base_preferences.dart';
 export 'src/helpers/preferences/preferences_generator.dart';
-export 'src/helpers/reusable/preferences_listener.dart';
 export 'src/helpers/reusable/custom_buttons.dart';
 export 'src/helpers/reusable/empty_bloc_listener.dart';
 export 'src/helpers/reusable/error_view.dart';
+export 'src/helpers/reusable/preferences_listener.dart';
 export 'src/helpers/reusable/underlined_button.dart';
 // Network
 export 'src/network/perform_async.dart';
